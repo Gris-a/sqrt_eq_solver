0f9e744b4ddf8a38135ef2b67f7bb89fb33b43e3
Завьялов Григорий Аркадьевич <zavialov.ga@phystech.edu>
2023-08-17 21:19:18 +0400

    разбил код на функции, перешёл на тип переменных double, поправил кодстайл.
    Не допилил функцию c_scanf

--- sqrt_eq_solver.cpp
+++ sqrt_eq_solver.cpp
@@ -4,48 +4,80 @@
 /*программма решает уравнение вида a*x**2+b*x+c=0, получая на вход коэффициенты a, b и c.
 Подразумевается, что коэффициенты и корни уравнения действительные числа*/
 
+void c_scanf(double *a, double *b, double *c); //ввод и проверка коэффициентов
+void ln_eq(double b, double c, double *x1); //решение линейного уравнения(если квадратное сводится к таковому)
+void sq_eq(double a, double b, double c, double *x1, double *x2); //решение квадратного уравнения
+
 int main(void)
 {
-    float a, b, c; //коэффициенты уравнения
-    float x1, x2; //корни уравнения
+    //коэффициенты
+    double a = 0;
+    double b = 0;
+    double c = 0;
+    //корни
+    double x1 = 0;
+    double x2 = 0;
 
-    printf("Введите коэффициенты квадратного уравнения через пробел: ");
-    scanf("%f %f %f", &a, &b, &c);
+    printf("Введите коэффициенты квадратного уравнения: ");
+    c_scanf(&a, &b, &c);
 
-    if (fabs(a) < __FLT_EPSILON__)
+    if (fabs(a) < __DBL_EPSILON__)
     {
-        if (fabs(b) < __FLT_EPSILON__)
-        {
-            if (fabs(c) < __FLT_EPSILON__)
-            {
-                printf("корнем уравнения может быть любое действительное число.\n");
-                return -1;
-            }
-            printf("Корней нет.\n");
-            return 0;
-        }
-        x1 = -(c / b);
-        printf("%f - корень уравнения.\n", x1);
-        return 1;
+        ln_eq(b, c, &x1);
     }
-
-    float D; //дискриминант
-    D = powf(b, 2) - 4 * a * c;
-
-    if (D < 0)
+    else
     {
-        printf("Корней нет.\n");
-        return 0;
-    }
-    if (fabs(D) < __FLT_EPSILON__)
-    {
-        x1 = -(b / (2 * a));
-        printf("%f - корень уравнения.\n", x1);
-        return 1;
-    }
+        sq_eq(a, b, c, &x1, &x2);
 
-    x1 = (-b - sqrtf(D)) / (2 * a);
-    x2 = (-b + sqrtf(D)) / (2 * a);
-    printf("%f и %f - корни уравнения\n", x1, x2);
-    return 2;
+    }
+    return 0;
+}
+
+void c_scanf(double *k1, double *k2, double *k3)
+{
+    while(1)
+    {
+        scanf("%lf", k1);
+        scanf("%lf", k2);
+        scanf("%lf", k3);
+        break;
+    }
+}
+
+void ln_eq(double b, double c, double *x1)
+{
+    if(fabs(b) < __DBL_EPSILON__ && fabs(c) < __DBL_EPSILON__)
+    {
+        printf("Корнем может быть любое действительное число\n");
+    }
+    else if(abs(b) < __DBL_EPSILON__ && fabs(c) >= __DBL_EPSILON__)
+    {
+        printf("Нет корней\n");
+    }
+    else
+    {
+        *x1 = - (c / b);
+        printf("%lf - корень уравнения\n", *x1);
+    }
+}
+
+void sq_eq(double a, double b, double c, double *x1, double *x2)
+{
+    double D = b * b - 4 * a * c; //дискриминант
+
+    if(fabs(D) < __DBL_EPSILON__)
+    {
+        *x1 = - (b / (2 * a));
+        printf("%lf - корень уравнения\n", *x1);
+    }
+    else if(D < 0)
+    {
+        printf("Нет корней\n");
+    }
+    else
+    {
+        *x1 = (-b - sqrt(D)) / (2 * a);
+        *x2 = (-b + sqrt(D)) / (2 * a);
+        printf("%lf и %lf - корни уравнения\n", *x1, *x2);
+    }
 }
