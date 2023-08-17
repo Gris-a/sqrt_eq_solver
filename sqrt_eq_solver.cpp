3440cda4c8bec04de9a502208ebc30f715514735
Завьялов Григорий Аркадьевич <zavialov.ga@phystech.edu>
2023-08-17 14:51:15 +0400

    использую math.h, а не cmath; изменил тип данных x1, x2 с doubleна float; использую встроенную константу __FLT_EPSILON__ для сравнения чисел типа  float; использую функцию powf и sqrtf

--- sqrt_eq_solver.cpp
+++ sqrt_eq_solver.cpp
@@ -1,5 +1,5 @@
 #include <stdio.h>
-#include <cmath>
+#include <math.h>
 
 /*программма решает уравнение вида a*x**2+b*x+c=0, получая на вход коэффициенты a, b и c.
 Подразумевается, что коэффициенты и корни уравнения действительные числа*/
@@ -7,16 +7,16 @@
 int main(void)
 {
     float a, b, c; //коэффициенты уравнения
-    double x1, x2; //корни уравнения
-    
+    float x1, x2; //корни уравнения
+
     printf("Введите коэффициенты квадратного уравнения через пробел: ");
     scanf("%f %f %f", &a, &b, &c);
 
-    if (fabs(a) < 0.0000001)
+    if (fabs(a) < __FLT_EPSILON__)
     {
-        if (fabs(b) < 0.0000001)
+        if (fabs(b) < __FLT_EPSILON__)
         {
-            if (fabs(c) < 0.0000001)
+            if (fabs(c) < __FLT_EPSILON__)
             {
                 printf("корнем уравнения может быть любое действительное число.\n");
                 return -1;
@@ -30,22 +30,22 @@ int main(void)
     }
 
     float D; //дискриминант
-    D = b * b - 4 * a * c;
+    D = powf(b, 2) - 4 * a * c;
 
     if (D < 0)
     {
         printf("Корней нет.\n");
         return 0;
     }
-    if (fabs(D) < 0.0000001)
+    if (fabs(D) < __FLT_EPSILON__)
     {
         x1 = -(b / (2 * a));
         printf("%f - корень уравнения.\n", x1);
         return 1;
     }
 
-    x1 = (-b - sqrt(D)) / (2 * a);
-    x2 = (-b + sqrt(D)) / (2 * a);
+    x1 = (-b - sqrtf(D)) / (2 * a);
+    x2 = (-b + sqrtf(D)) / (2 * a);
     printf("%f и %f - корни уравнения\n", x1, x2);
     return 2;
 }
