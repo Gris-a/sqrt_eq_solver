01cee1341c1ff72cc16d3b6e2c9329a865668a33
Завьялов Григорий Аркадьевич <zavialov.ga@phystech.edu>
2023-08-18 19:31:07 +0400

    сократил код

--- sqrt_eq_solver.cpp
+++ sqrt_eq_solver.cpp
@@ -4,7 +4,7 @@
 /*программма решает уравнение вида a*x**2+b*x+c=0, получая на вход коэффициенты a, b и c.
 Подразумевается, что коэффициенты и корни уравнения действительные числа*/
 
-void check_scanf(double *a, double *b, double *c); //ввод и проверка коэффициентов
+void check_scanf(double *k); //ввод и проверка коэффициентов
 void linear_equation(double b, double c); //решение линейного уравнения(если квадратное сводится к таковому)
 void quadratic_equation(double a, double b, double c); //решение квадратного уравнения
 void clean_buff(void); //очищает буфер для корректного считывания scanf
@@ -15,8 +15,14 @@ int main(void)
     double a = 0;
     double b = 0;
     double c = 0;
+    printf("Введите коэффициент перед x^2: ");
+    check_scanf(&a);
 
-    check_scanf(&a, &b, &c);
+    printf("Введите коэффициент перед x: ");
+    check_scanf(&b);
+
+    printf("Введите свободный член: ");
+    check_scanf(&c);
 
     if (fabs(a) < __DBL_EPSILON__)
     {
@@ -29,15 +35,14 @@ int main(void)
     return 0;
 }
 
-void check_scanf(double *a, double *b, double *c)
+void check_scanf(double *k)
 {
     char ncheck = '\0'; //проверка строки на \n после числа
     int scanf_out = 0;
 
-    printf("Введите коэффициент перед x^2: ");
     while(1)
     {
-        scanf_out = scanf("%lf%c", a, &ncheck);
+        scanf_out = scanf("%lf%c", k, &ncheck);
         if((scanf_out != 2) || (ncheck != '\n'))
         {
             printf("Некорректный ввод, попробуйте снова: ");
@@ -45,37 +50,7 @@ void check_scanf(double *a, double *b, double *c)
         }
         else
         {
-            break;
-        }
-    }
-
-    printf("Введите коэффициент перед x: ");
-    while(1)
-    {
-        scanf_out = scanf("%lf%c", b, &ncheck);
-        if((scanf_out != 2) || (ncheck != '\n'))
-        {
-            printf("Некорректный ввод, попробуйте снова: ");
-            clean_buff();
-        }
-        else
-        {
-            break;
-        }
-    }
-
-    printf("Введите свободный коэффициент: ");
-    while(1)
-    {
-        scanf_out = scanf("%lf%c", c, &ncheck);
-        if((scanf_out != 2) || (ncheck != '\n'))
-        {
-            printf("Некорректный ввод, попробуйте снова: ");
-            clean_buff();
-        }
-        else
-        {
-            break;
+            return;
         }
     }
 }
